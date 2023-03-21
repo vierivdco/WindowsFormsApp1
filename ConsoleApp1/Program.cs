39b20d6c499e8f4984a0d62a64a7fb7eb3a0939c
Alumno <Alumno@LAB707-02>
2023-03-20 22:17:01 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Procedimientos y funciones";
+            String opcion;
+            int a, b;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("[1] Suma de dos números");
+                Console.WriteLine("[2] Resta de dos números");
+                Console.WriteLine("[3] Producto de dos números");
+                Console.WriteLine("[4] Cociente de dos números");
+                Console.WriteLine("[5] imprimir la raíz cuadrada de los 10 primeros números enteros");
+                Console.WriteLine("[6] Los primeros 10 números primos");
+                Console.WriteLine("[7] Convertir grados C a F");
+                Console.WriteLine("[8] Convertir grados F a C");
+                Console.WriteLine("[0] Salir");
+                Console.WriteLine("Ingrese una opción y presione ENTER");
+                opcion = Console.ReadLine();
+                switch (opcion)
+                {
+                    case "1":
+                        Console.WriteLine("Ingrese el primer número");
+                        a = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Ingrese el segundo número");
+                        b = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("La suma de {0} y {1} es {2}", a, b, Suma(a, b));
+                        Console.ReadKey();
+                        break;
+                    case "2":
+                        Console.WriteLine("Ingrese el primer número");
+                        a = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Ingrese el segundo número");
+                        b = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("La diferencia de {0} y {1} es {2}", a, b, Resta(a, b));
+                        Console.ReadKey();
+                        break;
+                    case "3":
+                        Console.WriteLine("Ingrese el primer número");
+                        a = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Ingrese el segundo número");
+                        b = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("El producto de {0} y {1} es {2}", a, b, Multi(a, b));
+                        Console.ReadKey();
+                        break;
+                    case "4":
+                        Console.WriteLine("Ingrese el primer número");
+                        a = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Ingrese el segundo número");
+                        b = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("La división de {0} entre {1} es {2}", a, b, Division(a, b));
+                        Console.ReadKey();
+                        break;
+                    case "5":
+                        Console.WriteLine("Calculando...");
+                        Raiz();
+                        Console.ReadKey();
+                        break;
+                    case "6":
+                        Console.WriteLine("Calculando...");
+                        Primos();
+                        Console.ReadKey();
+                        break;
+                    case "7":
+                        Console.WriteLine("Ingrese el número de grados C");
+                        a = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("La conversión de {0} grados C a F es igual a {1}", a, Cel(a));
+                        Console.ReadKey();
+                        break;
+                    case "8":
+                        Console.WriteLine("Ingrese el número de grados F");
+                        a = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("La conversión de {0} grados F a C es igual a {1}", a, Far(a));
+                        Console.ReadKey();
+                        break;
+                }
+            } while (!opcion.Equals("0"));
+        }
+        static int Suma(int a, int b)
+        {
+            return a + b;
+        }
+        
+        static void Raiz()
+        {
+            for  (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine("La raíz cuadrada de {0} es: {1}",i,Math.Sqrt(i));
+            }
+        }
+
+        static int Resta(int a, int b)
+        {
+            return a - b;
+        }
+
+        static int Multi(int a, int b)
+        {
+            return a * b;
+        }
+
+        static int Division(int a, int b)
+        {
+            return a / b;
+        }
+
+        static void Primos() {
+            int cont = 0;
+            int n = 2;
+
+            while (cont < 10)
+            {
+                bool isPrime = true;
+
+                for (int i = 2; i <= Math.Sqrt(n); i++)
+                {
+                    if (n % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                {
+                    Console.Write(n + " ");
+                    cont++;
+                }
+
+                n++;
+            }
+
+        }
+
+        static int Cel(int a)
+        {
+            return ((9 * a) / 5) + 32;
+        }
+
+        static int Far(int a)
+        {
+            return (5 * (a - 32)) / 9;
+        }
+    }
+}
---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,367 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gboxDpto = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblCod = new System.Windows.Forms.LinkLabel();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtLastname = new System.Windows.Forms.TextBox();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtDni = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dtpBirth = new System.Windows.Forms.DateTimePicker();
+            this.label9 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DNI = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Apellidos = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Dirección = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Teléfono = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Fec_Nac = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Departamento = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.gboxDpto.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gboxDpto
+            // 
+            this.gboxDpto.Controls.Add(this.button1);
+            this.gboxDpto.Controls.Add(this.comboBox1);
+            this.gboxDpto.Controls.Add(this.label9);
+            this.gboxDpto.Controls.Add(this.dtpBirth);
+            this.gboxDpto.Controls.Add(this.txtDni);
+            this.gboxDpto.Controls.Add(this.label8);
+            this.gboxDpto.Controls.Add(this.txtEmail);
+            this.gboxDpto.Controls.Add(this.txtPhone);
+            this.gboxDpto.Controls.Add(this.txtDireccion);
+            this.gboxDpto.Controls.Add(this.txtLastname);
+            this.gboxDpto.Controls.Add(this.txtName);
+            this.gboxDpto.Controls.Add(this.lblCod);
+            this.gboxDpto.Controls.Add(this.label7);
+            this.gboxDpto.Controls.Add(this.label6);
+            this.gboxDpto.Controls.Add(this.label5);
+            this.gboxDpto.Controls.Add(this.label4);
+            this.gboxDpto.Controls.Add(this.label3);
+            this.gboxDpto.Controls.Add(this.label2);
+            this.gboxDpto.Controls.Add(this.label1);
+            this.gboxDpto.Location = new System.Drawing.Point(12, 13);
+            this.gboxDpto.Name = "gboxDpto";
+            this.gboxDpto.Size = new System.Drawing.Size(477, 258);
+            this.gboxDpto.TabIndex = 0;
+            this.gboxDpto.TabStop = false;
+            this.gboxDpto.Text = "Datos personales";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Código:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Apellido:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Dirección:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Teléfono:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(35, 184);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Email:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(7, 216);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(63, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Fecha Nac:";
+            // 
+            // lblCod
+            // 
+            this.lblCod.AutoSize = true;
+            this.lblCod.Location = new System.Drawing.Point(76, 25);
+            this.lblCod.Name = "lblCod";
+            this.lblCod.Size = new System.Drawing.Size(45, 13);
+            this.lblCod.TabIndex = 7;
+            this.lblCod.TabStop = true;
+            this.lblCod.Text = "U10023";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(76, 51);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 8;
+            // 
+            // txtLastname
+            // 
+            this.txtLastname.Location = new System.Drawing.Point(76, 85);
+            this.txtLastname.Name = "txtLastname";
+            this.txtLastname.Size = new System.Drawing.Size(100, 20);
+            this.txtLastname.TabIndex = 9;
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(76, 114);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(100, 20);
+            this.txtDireccion.TabIndex = 10;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(76, 150);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(100, 20);
+            this.txtPhone.TabIndex = 11;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(76, 184);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 12;
+            // 
+            // txtDni
+            // 
+            this.txtDni.Location = new System.Drawing.Point(329, 53);
+            this.txtDni.Name = "txtDni";
+            this.txtDni.Size = new System.Drawing.Size(100, 20);
+            this.txtDni.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(294, 56);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(29, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "DNI:";
+            // 
+            // dtpBirth
+            // 
+            this.dtpBirth.Location = new System.Drawing.Point(76, 216);
+            this.dtpBirth.Name = "dtpBirth";
+            this.dtpBirth.Size = new System.Drawing.Size(200, 20);
+            this.dtpBirth.TabIndex = 16;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(249, 88);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(77, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Departamento:";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(329, 84);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 18;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.DNI,
+            this.Nombre,
+            this.Apellidos,
+            this.Dirección,
+            this.Teléfono,
+            this.Email,
+            this.Fec_Nac,
+            this.Departamento});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 292);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(946, 150);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Código";
+            this.Column1.Name = "Column1";
+            // 
+            // DNI
+            // 
+            this.DNI.HeaderText = "DNI";
+            this.DNI.Name = "DNI";
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            // 
+            // Apellidos
+            // 
+            this.Apellidos.HeaderText = "Apellidos";
+            this.Apellidos.Name = "Apellidos";
+            // 
+            // Dirección
+            // 
+            this.Dirección.HeaderText = "Dirección";
+            this.Dirección.Name = "Dirección";
+            // 
+            // Teléfono
+            // 
+            this.Teléfono.HeaderText = "Teléfono";
+            this.Teléfono.Name = "Teléfono";
+            // 
+            // Email
+            // 
+            this.Email.HeaderText = "Email";
+            this.Email.Name = "Email";
+            // 
+            // Fec_Nac
+            // 
+            this.Fec_Nac.HeaderText = "Fec/Nac";
+            this.Fec_Nac.Name = "Fec_Nac";
+            // 
+            // Departamento
+            // 
+            this.Departamento.HeaderText = "Departamento";
+            this.Departamento.Name = "Departamento";
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.Blue;
+            this.button1.Location = new System.Drawing.Point(329, 216);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Agregar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(970, 533);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.gboxDpto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.gboxDpto.ResumeLayout(false);
+            this.gboxDpto.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gboxDpto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDni;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.TextBox txtLastname;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.LinkLabel lblCod;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.DateTimePicker dtpBirth;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DNI;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Apellidos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Dirección;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Teléfono;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Email;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Fec_Nac;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Departamento;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
